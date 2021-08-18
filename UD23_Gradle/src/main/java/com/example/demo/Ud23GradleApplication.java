589df7c30c0685301c86fca483f4ab5e36b54711
Dídac Montero Borràs <37572665+dimobo@users.noreply.github.com>
2021-08-18 17:06:11 +0200

    ok

---
+++ UD23_Gradle/src/main/java/com/example/demo/Ud23GradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ud23GradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ud23GradleApplication.class, args);
+	}
+
+}
